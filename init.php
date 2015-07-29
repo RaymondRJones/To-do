0305bd5cecdd7cee8a06d4d94f3d6306c0762f7e
Rayxfl <Rayjones2170@gmail.com>
2015-07-29 12:44:51 -0500

    init
    Description

---
+++ init.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_start();
+$username = "root";
+$password = "Maddog288722";
+$_SESSION['users'] = 1;
+try {
+$db = new PDO("mysql: host=127.0.0.1; dbname=todo", $username, $password);
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    }
+catch(PDOxception $e) {
+	echo "connection failed: " . $e->getMessage();
+
+}
+// Handle this in someother way
+?>
